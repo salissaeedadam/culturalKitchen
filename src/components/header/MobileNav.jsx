dba7533c7ea59c53ffb268d56cb3061c29d57e2e
Muhammad <jamiludeensani@gmail.com>
2024-12-14 17:41:51 +0100

    Add PropTypes for type checking and implement DesktopNav and MobileNav components
    - Added `prop-types` dependency to `package.json` and `package-lock.json`.
    - Refactored `DesktopNav` and `MobileNav` components to include PropTypes for better type validation.
    - Cleaned up commented-out code in both components for improved readability.
    - Updated `Header` component to utilize the new `DesktopNav` and `MobileNav` implementations with proper props.

--- src/components/header/MobileNav.jsx
+++ src/components/header/MobileNav.jsx
@@ -1,25 +1,37 @@
-// import React from 'react'
-// import { HiBars3BottomRight } from "react-icons/hi2"
-// import { RiCloseCirlceLine } from "react-icons/ri"
+import PropTypes from "prop-types";
+import { RiCloseCircleLine } from "react-icons/ri";
 
-// const MobileNav = ({menuItems, Logo, onClose, hideLeft, onOpen}) => {
-//   return (
-//     <div className='h-16 flex justify-between items-center px-6 lg:px-12'>
-//          <a href="/">
-//             <img src={Logo} alt="logo" />
-//         </a>
-//         <button onClick={onOpen} className='border border-primary rounded '>
-//             <HiBars3BottomRight className='w-7 h-7'/>
-//         </button>
+const MobileNav = ({ menuItems, Logo, onClose, hideLeft, onOpen }) => {
+  return (
+    <div
+      className={`transition-all w-full h-full fixed bg-primary z-50 top-0 ${hideLeft} flex flex-col items-center`}
+    >
+      <button onClick={onClose} className="absolute right-8 top-32">
+        <RiCloseCircleLine className="w-7 h-7" />
+      </button>
+      <button onClick={onOpen} className="mt-4">
+        Open Menu
+      </button>
+      <div className="mt-8">
+        <a href="/">
+          <img src={Logo} alt="logo" />
+        </a>
+        {menuItems.map((item, index) => (
+          <a key={index} href={`/${item}`} className="block py-2">
+            {item}
+          </a>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-//         <div className={`transition-all w-full h-full fixed bg-primary z-50 top-0 ${hideLeft} flex justify-center items-center`}>
+MobileNav.propTypes = {
+  menuItems: PropTypes.arrayOf(PropTypes.string).isRequired,
+  Logo: PropTypes.string.isRequired,
+  onClose: PropTypes.func.isRequired,
+  hideLeft: PropTypes.string.isRequired,
+  onOpen: PropTypes.func.isRequired,
+};
 
-//           <button onClick={onClose} className='absolute right-8 top-32'>
-//             <RiCloseCirlceLine className='w-7 h-7'/>
-//           </button>
-//         </div>
-//     </div>
-//   )
-// }
-
-// export default MobileNav
+export default MobileNav;
